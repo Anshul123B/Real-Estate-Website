73ef1f2a5e97975aedf0b6e2ea92bb3f2a5fc793
Anshul Bhatt <anb201982@gmail.com>
2025-04-18 23:04:18 +0530

    updated

--- Real-Estate-Website/src/components/Residencies/Residencies.jsx
+++ Real-Estate-Website/src/components/Residencies/Residencies.jsx
@@ -3,6 +3,7 @@ import {Swiper, SwiperSlide, useSwiper} from 'swiper/react'
 import "swiper/css"
 import './Residencies.css'
 import data from '../../utils/slider.json'
+import { sliderSettings } from "../../utils/common";
 
 const Residencies = () => {
   return (
@@ -13,7 +14,8 @@ const Residencies = () => {
                 <span className='primaryText'>Popular Residencies</span>
             </div>
 
-            <Swiper>
+            <Swiper{...sliderSettings}>
+              <SliderButtons/>
                 {
                   data.map((card, i) => (
                     <SwiperSlide key={i}>
@@ -34,7 +36,17 @@ const Residencies = () => {
             </Swiper>
         </div>
     </section>
-  )
-}
+  );
+};
 
 export default Residencies
+
+const SliderButtons = () =>{
+  const swiper = useSwiper();
+  return(
+    <div className="flexCenter r-button">
+      <button onClick={()=>swiper.slidePrev()}>&lt;</button>
+      <button onClick={()=>swiper.slideNext()}>&gt;</button>
+    </div>
+  );
+};
--- Real-Estate-Website/src/utils/common.jsx
+++ Real-Estate-Website/src/utils/common.jsx
@@ -1,9 +1,20 @@
 import { CgSpaceBetween } from "react-icons/cg";
 
-export const sliderSetting = {
-    slidePerView:1,
-    spaceBetween: 50,
-    breakpoints:{
-        
+export const sliderSettings = {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+    },
+    600: {
+      slidesPerView: 2,
+    },
+    750: {
+      slidesPerView: 3,
+    },
+    1100: {
+      slidesPerView: 4,
     }
-}
+  }
+};
