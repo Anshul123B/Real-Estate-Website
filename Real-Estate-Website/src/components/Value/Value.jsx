de277cff63badcada68224b51f63b39d304c4caa
Anshul Bhatt <anb201982@gmail.com>
2025-04-19 12:21:54 +0530

    updated value.jsx

--- Real-Estate-Website/src/components/Value/Value.jsx
+++ Real-Estate-Website/src/components/Value/Value.jsx
@@ -15,7 +15,29 @@ import './value.css'
 const Value = () => {
   return (
     <div>
-      value
+      <section className="v-wrapper">
+        <div className="padding innerWidth flexCenter v-vontainer">
+            {/*left side */}
+            <div className="v-left">
+                <div className="image-container">
+                    <img src="./value.png" alt=""/>
+                </div>
+            </div>
+
+            {/*right side*/}
+            <div className="felxColStart v-right">
+                <span className="orangeText">Our Value</span>
+                <br/>
+                <span className="primaryText">Value We Give to you</span>
+                <br/>
+                <span className='secondaryText'>
+                      We're always here to support you with top-notch service.
+                      <br />
+                      We believe the right place to live can truly enhance your life.
+                </span>
+            </div>
+        </div>
+      </section>
     </div>
   )
 }
