9ee32a6e335cce5604275a6b0a845cb9911605af
Anshul Bhatt <anb201982@gmail.com>
2025-04-19 12:08:48 +0530

    updated

---
+++ Real-Estate-Website/src/components/Value/Value.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemHeading,
+    AccordionItemButton,
+    AccordionItemPanel,
+    AccordionItemState
+  } from "react-accessible-accordion";
+  
+import "react-accessible-accordion/dist/fancy-example.css";
+import { MdOutlineArrowDropDown } from 'react-icons/md'
+import './value.css'
+
+const Value = () => {
+  return (
+    <div>
+      value
+    </div>
+  )
+}
+
+export default Value
